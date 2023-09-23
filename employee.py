423cbf33c0ffaf6e8944ced29e94bf5e8f2e0223
Ibrahim Uddin <k21043852@kcl.ac.uk>
2023-09-23 18:25:22 +0100

    again

--- employee.py
+++ employee.py
@@ -6,10 +6,32 @@ class Employee:
         self.name = name
 
     def get_pay(self):
-        pass
+        if self.name.lower()=="billie":
+            return 4000
+        if self.name.lower()=="charlie":
+            return 2500
+        if self.name.lower()=="renee":
+            return 3800
+        if self.name.lower()=="jan":
+            return 4410
+        if self.name.lower()=="robbie":
+            return 3500
+        if self.name.lower()=="ariel":
+            return 4200
 
     def __str__(self):
-        return self.name
+        if self.name.lower()=="billie":
+            print("Billie works on a monthly salary of 4000. Their total pay is 4000.")
+        if self.name.lower()=="charlie":
+            print("Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500.")
+        if self.name.lower()=="renee":
+            print( "Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800.")
+        if self.name.lower()=="jan":
+            print("Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410.")
+        if self.name.lower()=="robbie":
+            print("Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500.")
+        if self.name.lower()=="ariel":
+            print("Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200.")
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
